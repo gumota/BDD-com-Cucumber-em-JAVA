05772c725461d8f04d9edc3b900794f5df4f79b7
Gustavo Mota <gustavo.omota@hotmail.com>
2020-03-27 21:06:54 -0300

    New

--- src/test/java/AprenderCucumber.java
+++ src/test/java/AprenderCucumber.java
@@ -1,26 +1,80 @@
-import cucumber.api.PendingException;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import static org.junit.Assert.assertEquals;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Então;
+import cucumber.api.java.pt.Quando;
 
 public class AprenderCucumber {
 
-	@Given("^que criei o arquivo corretamente$")
-	public void que_criei_o_arquivo_corretamente() throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+//	Passou, falhou, skipped, pending e undefined - 5 estados que existe no cucumber
+
+	@Dado("^que criei o arquivo corretamente$")
+	public void queCrieiOArquivoCorretamente() throws Throwable {
+		System.out.println("Gugu");
 	}
 
-	@When("^executá-lo$")
-	public void executá_lo() throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+	@Quando("^executá-lo$")
+	public void executáLo() throws Throwable {
 	}
 
-	@Then("^a especificação deve finalizar com sucesso$")
-	public void a_especificação_deve_finalizar_com_sucesso() throws Throwable {
-		// Write code here that turns the phrase above into concrete actions
-		throw new PendingException();
+	@Então("^a especificação deve finalizar com sucesso$")
+	public void aEspecificaçãoDeveFinalizarComSucesso() throws Throwable {
+	}
+
+	private int contador = 0;
+
+	@Dado("^que o valor do contador é (\\d+)$")
+	public void queOValorDoContadorÉ(int arg1) throws Throwable {
+		contador = arg1;
+	}
+
+	@Quando("^eu incrementar em (\\d+)$")
+	public void euIncrementarEm(int arg1) throws Throwable {
+		contador = contador + arg1;
+	}
+
+	@Então("^o valor do contador será (\\d+)$")
+	public void oValorDoContadorSerá(int arg1) throws Throwable {
+		assertEquals(arg1, contador);
+
+	}
+
+	Date entrega = new Date();
+
+	@Dado("^que a entrega é dia (\\d{2})/(\\d{2})/(\\d{4})$")
+	public void queAEntregaÉDia(int dia, int mes, int ano) throws Throwable {
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.DAY_OF_MONTH, dia);
+		cal.set(Calendar.MONTH, mes - 1);
+		cal.set(Calendar.YEAR, ano);
+		entrega = cal.getTime();
+	}
+
+	@Quando("^a entrega atrasar em (\\d+) (dia|dias|mes|meses)$")
+	public void aEntregaAtrasarEmDias(int tempo, String unidade) throws Throwable {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(entrega);
+		
+		if(unidade.equals("meses") || unidade.equals("mes")) {
+			cal.add(Calendar.MONTH, tempo);
+		} 
+		if(unidade.equals("dias") || unidade.equals("dia")){
+			cal.add(Calendar.DAY_OF_MONTH, tempo);
+		}
+		entrega = cal.getTime();
+	}
+
+	@Então("^a entrega será efetuada em (\\d{2}\\/\\d{2}\\/\\d{4})$")
+	public void aEntregaSeráEfetuadaEm(String data) throws Throwable {
+		DateFormat format = new SimpleDateFormat("dd/MM/yyyy");
+		String dataFormatada = format.format(entrega);
+		assertEquals(data, dataFormatada);
+
 	}
 
 }
--- src/test/java/aprender_cucumber.feature
+++ src/test/java/aprender_cucumber.feature
@@ -1,9 +1,31 @@
-Feature: Aprender Cucumber
+# language: pt
+Funcionalidade: Aprender Cucumber
 	Como um aluno
 	Eu quero aprender a utilizar Cucumber
 	Para que eu possa automatizar critérios de aceitação
 
-Scenario: Deve executar especificação
-	Given que criei o arquivo corretamente
-	When executá-lo
-	Then a especificação deve finalizar com sucesso 
+Cenário: Deve executar especificação
+	Dado que criei o arquivo corretamente
+	Quando executá-lo
+	Então a especificação deve finalizar com sucesso
+
+
+Cenario: Deve incrementar contador
+	Dado que o valor do contador é 15
+	Quando eu incrementar em 3
+	Então o valor do contador será 18
+	
+Cenario: Deve incrementar contador
+	Dado que o valor do contador é 123
+	Quando eu incrementar em 35
+	Então o valor do contador será 158
+	
+Cenario: Deve calcular atraso na entrega
+	Dado que a entrega é dia 05/04/2018
+	Quando a entrega atrasar em 2 dia
+	Então a entrega será efetuada em 07/04/2018
+	
+Cenario: Deve calcular atraso na entrega da China
+	Dado que a entrega é dia 05/04/2018
+	Quando a entrega atrasar em 2 meses
+	Então a entrega será efetuada em 05/06/2018
